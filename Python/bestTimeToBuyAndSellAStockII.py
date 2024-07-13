ecad0f58a17b4fd2a9ff778cd861f77678f2fda3
Cr1mson901 <o_devon2@yahoo.com>
2024-07-13 14:55:59 -0400

    Just an Extension of the previous problem

--- Python/bestTimeToBuyAndSellAStockII.py
+++ Python/bestTimeToBuyAndSellAStockII.py
@@ -1,11 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         bp = float('inf')
-        total_profit = 0
+        max_profit = 0
         for price in prices:
             if price < bp:
                 bp = price
-            else:
-                total_profit += price - bp
-                bp = price
-        return total_profit
+            elif price - bp > max_profit:
+                max_profit = price - bp
+        return max_profit
