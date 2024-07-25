0e3a65aeb143833f8aa874275dfc6e458ff50d46
Cr1mson901 <o_devon2@yahoo.com>
2024-07-25 13:42:16 -0400

    updated hashmap name from camelCase to snake_case

--- Python/Hashmap/containsDuplicateII.py
+++ Python/Hashmap/containsDuplicateII.py
@@ -1,20 +1,20 @@
 # Easy solution but not that fast
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        myMap = {}
+        my_map = {}
         for idx, num in enumerate(nums):
-            if num in myMap and idx - myMap[num] <= k:
+            if num in my_map and idx - my_map[num] <= k:
                 return True
             else:
-                myMap[num] = idx
+                my_map[num] = idx
         return False
 # Apparently enumerate is slightly slower
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        myMap = {}
+        my_map = {}
         for i in range(len(nums)):
-            if nums[i] in myMap and i - myMap[nums[i]] <= k:
+            if nums[i] in my_map and i - my_map[nums[i]] <= k:
                 return True
             else:
-                myMap[nums[i]] = i
+                my_map[nums[i]] = i
         return False
