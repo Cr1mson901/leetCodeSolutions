b595ae90aab166795db6967d8fa655a158af6035
Cr1mson901 <o_devon2@yahoo.com>
2024-07-26 12:29:55 -0400

    Added a second solution that was condensed down

--- Python/Stack/validParentheses.py
+++ Python/Stack/validParentheses.py
@@ -16,3 +16,14 @@ class Solution:
             return True
         else:
             return False
+#Use the len comparison as the return and check against the pop directly
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        starts = {"(":")","{":"}","[":"]"}
+        for bracket in s:
+            if bracket in starts:
+                stack.append(starts[bracket])
+            elif len(stack) == 0 or bracket!=stack.pop():
+                return False
+        return len(stack) == 0
