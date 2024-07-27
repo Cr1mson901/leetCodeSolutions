deb2244f25f844db471b543f9ff3a4382c3afbe6
Cr1mson901 <o_devon2@yahoo.com>
2024-07-26 20:33:25 -0400

    I hate calculators

--- Python/Stack/basicCalculator.py
+++ Python/Stack/basicCalculator.py
@@ -1,20 +1,40 @@
+# Closer but I despise double negatives
 class Solution:
     def calculate(self, s: str) -> int:
-        numbers = [x for x in s if x!="(" and x!=")" and x!=" "]
-        output = []
-        function = 1
-        operators = ("-","+")
+        numbers = [x for x in s if x!=" "]
+        operators = {"+":int.__add__,"-":int.__sub__,"(":0,")":0}
+        op_stack = []
+        stack = []
+        output = [0]
         i = 0
         while i < len(numbers):
-            if numbers[i] in operators:
-                if numbers[i] == "-":
-                    function *= -1
-                i += 1
-            else:    
+            if numbers[i] not in operators:
                 j = i + 1
                 while j < len(numbers) and numbers[j] not in operators:
                     j += 1
-                output.append(function * int("".join(numbers[i:j])))
+                stack.append(int("".join(numbers[i:j])))
                 i = j
-                function = 1
-        return sum(output)
+                if len(op_stack) > 0 and op_stack[-1] != "(":
+                    stack.append(op_stack.pop())
+                continue
+            elif len(op_stack) == 0 or numbers[i] == "(":
+                op_stack.append(numbers[i])
+            elif numbers[i] == ")":
+                while op_stack[-1] != "(":
+                    stack.append(op_stack.pop())
+                op_stack.pop()
+            else:
+                op_stack.append(numbers[i])
+            i += 1
+        while len(op_stack) > 0:
+            stack.append(op_stack.pop())
+        for i in range(len(stack)):
+            if stack[i] in operators.keys() and len(output) > 1:
+                num2 = output.pop()
+                num1 = output.pop()
+                output.append(int(operators[stack[i]](num1,num2)))
+            elif stack[i] == "-":
+                stack[i+1] *= -1
+            else:
+                output.append(int(stack[i]))
+        return output[-1]
