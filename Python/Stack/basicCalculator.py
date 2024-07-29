7d65f9c48f81022bbc496ea4e4904a061669bdfa
Cr1mson901 <o_devon2@yahoo.com>
2024-07-29 09:31:54 -0400

    Added a real answer to the problem besides what I did

--- Python/Stack/basicCalculator.py
+++ Python/Stack/basicCalculator.py
@@ -46,3 +46,27 @@ class Solution:
             else:
                 output.append(int(stack[i]))
         return output[-1]
+    
+# This is a basic calculator. Not what I did
+class Solution2:
+  def calculate(self, s: str) -> int:
+    ans = 0
+    num = 0
+    sign = 1
+    stack = [sign]  # stack[-1]: current env's sign
+
+    for c in s:
+      if c.isdigit():
+        num = num * 10 + (ord(c) - ord('0'))
+      elif c == '(':
+        stack.append(sign)
+      elif c == ')':
+        stack.pop()
+      elif c == '+' or c == '-':
+        ans += sign * num
+        sign = (1 if c == '+' else -1) * stack[-1]
+        num = 0
+
+    return ans + sign * num
+  
+Solution2.calculate(Solution2,"(1+(4+5+2)-3)+(6+8)")
