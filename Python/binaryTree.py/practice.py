1fc942d1bf6d1cdb4ec24436925db7c3bae42419
Cr1mson901 <o_devon2@yahoo.com>
2024-07-29 14:17:14 -0400

    Added insertion function

--- Python/binaryTree.py/practice.py
+++ Python/binaryTree.py/practice.py
@@ -4,3 +4,18 @@ class Node:
         self.left = None
         self.right = None
         self.data = data
+    #Function for inserting data. Less then goes to the left. Greater then on the right
+    def insert(self, data):
+        if self.data is None:
+            self.data = data
+        else:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node
+                else:
+                    self.right.insert(data)
