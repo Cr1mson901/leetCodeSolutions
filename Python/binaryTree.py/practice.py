000f44b84bee9209f91221cb77777680c4cb185e
Cr1mson901 <o_devon2@yahoo.com>
2024-07-29 14:50:47 -0400

    Added two ways to print the tree

--- Python/binaryTree.py/practice.py
+++ Python/binaryTree.py/practice.py
@@ -16,6 +16,41 @@ class Node:
                     self.left.insert(data)
             elif data > self.data:
                 if self.right is None:
-                    self.right = Node
+                    self.right = Node(data)
                 else:
                     self.right.insert(data)
+
+    # def print(self):
+    #     # An Interesting way to display the heirarchy of the tree
+    #     # receipt = []
+    #     # if self.left:
+    #     #     receipt.append(self.left.print())
+    #     # receipt.append(self.data)
+    #     # if self.right:
+    #     #     receipt.append(self.right.print())
+    #     # return receipt
+
+def inOrderPrint(r):
+    if r is None:
+        return
+    else:
+        inOrderPrint(r.left)
+        print(r.data, end=" ")
+        inOrderPrint(r.right)
+
+
+
+    
+
+root = Node('g')
+root.insert('c')
+root.insert('b')
+root.insert('a')
+root.insert('e')
+root.insert('d')
+root.insert('f')
+root.insert('i')
+root.insert('h')
+root.insert('j')
+root.insert('k')
+inOrderPrint(root)
