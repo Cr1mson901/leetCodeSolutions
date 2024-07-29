e01c29feb2956ab204be1299919a710b4c7a8a86
Cr1mson901 <o_devon2@yahoo.com>
2024-07-29 11:06:17 -0400

    Created a length function and a removal function

--- Python/linkedLists.py/practice.py
+++ Python/linkedLists.py/practice.py
@@ -31,9 +31,43 @@ class LinkedList:
                 itr = itr.next
             itr.next = Node(data)
 
+    def insert_values(self,values=[]):
+        self.head = None
+        # Can reverse and use insert_at_end or regular and insert_at_beginning
+        for value in reversed(values):
+            self.insert_at_beginning(value)
+
+    def get_length(self):
+        itr = self.head
+        count = 0
+        while itr:
+            count += 1
+            itr = itr.next
+        return count
+    
+    def remove_at(self,idx):
+        if idx >= self.get_length():
+            raise Exception("Indexing Error")
+        elif idx == 0:
+            self.head = self.head.next
+        else:    
+            itr = self.head
+            for i in range(idx):
+                if i == idx-1:
+                    pre = itr
+                itr = itr.next
+            pre.next = itr.next
+ 
 test = LinkedList()
 test.insert_at_beginning(8)
 test.insert_at_beginning(9)
 test.print()
 test.insert_at_end(89)
+test.print()
+test.insert_values([1,2,3,4,65])
+test.print()
+print(test.get_length())
+test.remove_at(0)
+test.print()
+test.remove_at(1)
 test.print()
