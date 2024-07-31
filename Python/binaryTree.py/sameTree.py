0d0a6149c22cf495ffc338f710431a0cc2b15762
Cr1m <o_devon2@yahoo.com>
2024-07-31 13:21:34 -0400

    Compressed one of the if statements

--- Python/binaryTree.py/sameTree.py
+++ Python/binaryTree.py/sameTree.py
@@ -19,3 +19,19 @@ class Solution:
         if not Solution.isSameTree(Solution,p.right,q.right):
             return False
         return True
+    
+#Removed one of the if statements. Gotta love try
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        try:
+            if p.val != q.val:
+                return False
+        except:
+            return False
+        if not Solution.isSameTree(Solution,p.left,q.left):
+            return False
+        if not Solution.isSameTree(Solution,p.right,q.right):
+            return False
+        return True
