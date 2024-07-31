f364c76a534333ca43df8a6d1d0a99729e3e66a3
Cr1m <o_devon2@yahoo.com>
2024-07-31 14:16:06 -0400

    Added nested code solution

--- Python/binaryTree.py/symmetricTree.py
+++ Python/binaryTree.py/symmetricTree.py
@@ -20,3 +20,17 @@ class Solution:
         elif not root or not root2:
             return False
         return (self.isSymmetric(root.left, root2.right, first) and self.isSymmetric(root.right, root2.left,first))
+    
+#Much nicer code with a nested function
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode], root2=None, first=True) -> bool:
+        def isMirrored(left,right):
+            if not left and not right:
+                return True
+            elif not left or not right:
+                return False
+            else:
+                return (left.val == right.val 
+                and isMirrored(left.left,right.right) 
+                and isMirrored(left.right, right.left))
+        return isMirrored(root,root)
